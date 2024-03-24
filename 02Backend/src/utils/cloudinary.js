706b462534fad932bee9345952a4b065567735a0
devpatel66 <devpatel16@outlook.ingit config --global user.email devpatel16@outlook.ingit config --global user.email devpatel16@outlook.in>
2024-03-24 23:34:04 +0530

    configuare multer and cloudinary for file upload

---
+++ 02Backend/src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from 'cloudinary'
+import { log } from 'console';
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uplaodCloudinary = async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+
+        //uploading file to cloudinary
+        const resposne = await cloudinary.uploader.upload(localFilePath, {
+            resource_type:"auto"
+        })
+
+        console.log("File Uploaded to Cloudinary successfully", resposne.url);
+
+        return response;
+    } catch (error) {
+        //remove the locally saved temporary file as the uplaod operation got failed.
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+
+export default uplaodCloudinary 
